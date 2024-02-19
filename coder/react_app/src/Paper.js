5ca4dd7a6fb5161238247e7c2db01716881972e1
hugh park <phw0228@gmail.com>
2024-02-19 14:36:27 +0000

    init

---
+++ coder/react_app/src/Paper.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from "react";
+import Board from "./BoardComp/Board"
+
+export default function Paper(){
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
